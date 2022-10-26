91c91d95e6233202ad3badd1c486a7b776d5ee22
Skaiste Kankalyte <skaiste@Skaistes-MBP.lan>
2022-10-26 09:43:34 +0300

    Cards

---
+++ src/components/Cards/CardsSection.tsx
@@ -0,0 +1,29 @@
+import React, {FunctionComponent} from 'react';
+import styled from 'styled-components/native';
+import CardItem from './CardItem';
+import { CardSectionProps } from './types';
+
+ const CardList = styled.FlatList`
+   width: 100%;
+   flex: 1;
+   padding-left: 25px;
+   padding-bottom: 15px;
+ `
+
+const CardsSection: FunctionComponent<CardSectionProps> = props => {
+  return (
+    <CardList 
+      data= {props.data}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        padding: 25,
+        alignItems: 'center',
+      }}
+      keyExtractor={({id}: any) => id.toString()}
+      renderItem={({item}: any) => <CardItem {...item} />}
+    />
+  )
+}
+
+export default CardsSection;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,11 @@
-import {FunctionComponent} from 'react';
+import React, {FunctionComponent} from 'react';
 import {StatusBar} from 'react-native';
 import styled from 'styled-components/native';
 import {Container} from '../components/shared';
 import {colors} from '../components/colors';
+import logo1 from '../../assets/cards/visa_white.png';
+import logo2 from '../../assets/cards/mc.png';
+import CardsSection from '../components/Cards/CardsSection';
 
 const HomeContainer = styled(Container)`
   background-color: ${colors.graylight};
@@ -11,9 +14,33 @@ const HomeContainer = styled(Container)`
 `;
 
 const Home: FunctionComponent = () => {
+  const cardsData = [
+    {
+      id: 1,
+      accountNo: '75937694850',
+      balance: 29999.45,
+      alias: 'Work Debit',
+      logo: logo1,
+    },
+    {
+      id: 2,
+      accountNo: '86037602705',
+      balance: 29999.45,
+      alias: 'Personal Prepaid',
+      logo: logo2,
+    },
+    {
+      id: 3,
+      accountNo: '2946502750',
+      balance: 29999.45,
+      alias: 'School Prepaid',
+      logo: logo1,
+    },
+  ];
   return (
     <HomeContainer>
       <StatusBar barStyle="dark-content" />
+      <CardsSection data={cardsData} />
     </HomeContainer>
   );
 };
